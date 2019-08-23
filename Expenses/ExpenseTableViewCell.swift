6273290fca7d86a23a6511288cbcecdf81e8f912
Carmel Braga <carmelbraga@Carmels-MacBook-Air.local>
2019-08-23 16:49:23 -0500

    Error Fixed

--- Expenses/ExpenseTableViewCell.swift
+++ Expenses/ExpenseTableViewCell.swift
@@ -10,14 +10,14 @@ import UIKit
 
 class ExpenseTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var amountLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var amountLabel: UILabel!
-    @IBOutlet weak var dateLabel: UILabel!
     
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
